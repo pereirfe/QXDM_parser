c01eb48e867d917c720850c2d81e19c0a40c146a
Fernando Pereira <fernandhenriqp@gmail.com>
2017-01-05 00:14:16 -0200

    BugFixes. Version 0.1 working

--- QXDM_parser.py
+++ QXDM_parser.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
+# -*- coding: latin-1 -*-
 import sys
 import re
 import threading
 import time
 from subprocess import check_output
 import math
+from pprint import pprint
 
 linenum = 0
 
@@ -17,26 +19,30 @@ def progressing(filename):
     print "Progression: ",
 
     while linenum < total:
-        psize = math.floor(100*float(linenum)/float(total))
+        psize = int(math.floor(100*float(linenum)/float(total))/2)
 
-        print '█' * psize,
-        print ' ' * (100-psize),
-        print '| ', psize, "%",
+        print '█' * psize, ' ' * (50-psize-2), '| ',
+        if psize*2 < 10:
+            print ' ', psize*2, "%",
+        else:
+            print psize*2, "%",
 
-        time.sleep(5)
-        print '\b'*105
-
-
-
+        time.sleep(0.5)
+        print '\b'*59,
 
 
 def main():
+
+    global linenum
+    line_offset = 5000
+    enable_progress = True
+
     input_filename = sys.argv[1]
     output_filename = input_filename + "_PARSED.txt"
 
     codes_search = ["0xB18B"] #ToDo: Generalize, use a JSON
 
-    t_found = [ [] for x in codes_search ]
+#    t_found = [ [] for x in codes_search ]
     intervals_found = [ [] for x in codes_search ]
     events_found = [ [] for x in codes_search ]
 
@@ -58,9 +64,8 @@ def main():
                        "Current Subframe Number"
                      ]
 
-    state = "default"
+    state = "offset"
 
-    current_code = 0
     substage = 0
     candidate_code = 0
     time_compiled = 0
@@ -71,66 +76,85 @@ def main():
 
     code_extractor  = re.compile('0x[A-Fa-f0-9][A-Fa-f0-9][A-Fa-f0-9][A-Fa-f0-9]')
     value_extractor = re.compile('\d+')
+    block_matcher   = re.compile('\d\d\d\d [A-Za-z][A-Za-z][A-Za-z]')
 
-    progression_thread = threading.Thread(target=progressing, args=(filename))
-    progression_thread.start()
+    if enable_progress:
+        progression_thread = threading.Thread(target=progressing, args=(input_filename,))
+        progression_thread.start()
 
     with open(input_filename, 'r') as infile, open(output_filename, 'w') as outfile:
         for line in infile:
             linenum += 1
+            if state == "offset":
+                if linenum > line_offset:
+                    state = "default"
+                else:
+                    continue
 
             if state == "default":
-                if line[0:2] != '201':              # New block
+                if block_matcher.match(line):       # New block
                     candidate_code = (code_extractor.findall(line))[0]
                     block_finished = 0
                     for code in codes_search:
                         if code == candidate_code:
                             state = "code_found"
+                            current_code = candidate_code
 
                 else:                               # Inside a non-searched Block
                     if not block_finished:              # Used to block processing after getting info
                         for fm in frame_markers:            # Detect if line is a frame
                             if fm in line:
                                 time_compilation_steps += 1
-                                time_compiled += 10*value_extractor.findall(line)[-1]
+                                time_compiled += 10*int(value_extractor.findall(line)[-1])
 
                         for sfm in sframe_markers:          # Detect if line is a subframe
                             if sfm in line:
                                 time_compilation_steps += 1
-                                time_compiled += 1*value_extractor.findall(line)[-1]
+                                time_compiled += 1*int(value_extractor.findall(line)[-1])
 
                         if time_compilation_steps == 2:                 # when both frame and subframe were found
-                            for i in range(intervals_found):
-                                intv = intervals_found[i]
-                                if intv[0] <= time_compiled and intv[1] >= time_compiled:
-                                    events_found[i].append(candidate_code)
-                                    candidate_code = 0
+                            for cd_idx in range(len(codes_search)):
+                                for i in range(len(intervals_found[cd_idx])):  # ToDo: Fix. Not considering multiple codes
+                                    intv = intervals_found[cd_idx][i]
+                                    # print intv, i, len(intervals_found), intervals_found
+                                    if intv[0] <= time_compiled and intv[1] >= time_compiled:
+                                        events_found[cd_idx].append(candidate_code)
 
+                            candidate_code = 0
                             block_finished = 1
                             time_compilation_steps = 0
                             time_compiled = 0
 
             if state == "code_found":
                 if codes_markers[current_code][substage] in line:
-                    arg.append(int(filter(str.isdigit, line)))
+                    arg.append(int(value_extractor.findall(line)[-1]))
+                    # print "CODE FOUND: ", current_code, "\nVALUE: ", int(value_extractor.findall(line)[-1]), "\nLINENUM:", linenum
+                    # raw_input("FOUND")
                     substage += 1
 
                     if substage == len(codes_markers[current_code])-1: #last one which is not an instruction
+                        v = [0, 0]
+
+                        for i in range(len(codes_markers[current_code][-1][0])):
+                            v[0] += codes_markers[current_code][-1][0][i]*arg[i]
+
+                        for i in range(len(codes_markers[current_code][-1][1])):
+                            v[1] += codes_markers[current_code][-1][1][i]*arg[i]
+
+                        arg = []
                         substage = 0
-                        v = []
-                        for i in range(len(codes_markers[current_code][-1])):           # Number of outputs
-                            for j in range(len(codes_markers[current_code][-1][0])      # For each output, iterate over multipliers
-                                v[i] = codes_markers[current_code][-1][i][j]*arg[j]
 
-                        intervals_found.append(v)                                       # Interval Created
+                        intervals_found[codes_search.index(current_code)].append(v)      # Interval Created
                         state = "default"
-                        #Todo: How to handle this generically?
 
-                if line[0] == '201':             # Detect the end of an Inside Block
-                    state = "default"
-                    continue
+                # if line[0] == '201':             # Detect the end of an Inside Block
+                    # state = "default"
+                    # continue
 
-        print >> outfile, t_found, intervals_found, events_found
+        print >> outfile, "Intervals Found: "
+        pprint(intervals_found, stream=outfile)
+        print >> outfile, "Events Found: "
+        pprint(events_found, stream=outfile)
 
 if __name__ == "__main__":
     main()
