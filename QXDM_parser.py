d472670533e9429c831f865ceace60bd4f6f46b4
Fernando Pereira <fernandhenriqp@gmail.com>
2017-03-09 18:54:26 -0300

    10k+  loop sorting bug fixed
    Included a counter to make minimal size of chunk

--- QXDM_parser.py
+++ QXDM_parser.py
@@ -12,6 +12,16 @@ from pprint import pprint
 
 linenum = 0
 
+def timeToMilisseconds(timestring):
+    ''' Converts the timestring in the format HH:MM:SS.SSS to a integer
+    in milisseconds '''
+
+    t = int(timestring[9:12])
+    t += int(timestring[6:8])
+    t += int(timestring[3:5])
+    t += int(timestring[0:2])
+
+    return t
 
 def wc(filename):
     return int(check_output(["wc", "-l", filename]).split()[0])
@@ -56,6 +66,7 @@ def main():
     state = "offset"
 
     tl_time = []
+    tl_hour = []
     tl_code = []
     tl_string = []
 
@@ -66,6 +77,7 @@ def main():
     table_matchline = re.compile('^\s+\|[^\|]*\d+[^\|]*\|')
     table_string_extractor = re.compile('\|[^\|]+')
     block_matcher   = re.compile('^\d\d\d\d\s[A-Z][a-z][a-z]\s\d\d')
+    hour_extractor  = re.compile('\d\d:\d\d:\d\d.\d\d\d')
 
     data_type = []
     f_regex  = []
@@ -75,6 +87,7 @@ def main():
     info_string = []
     code = 0
     n_of_rec = -1
+    hour_log = 0
     
     table_times = []
     table_strings = []
@@ -106,12 +119,14 @@ def main():
                     block_processing = False
                     if data_type[-1] == "Data":
                         tl_time = tl_time + time
+                        tl_hour = tl_hour + [hour_log for i in spec[code]]
                         tl_code = tl_code + [code for i in spec[code]]
                         tl_string = tl_string + info_string
                             
                     if data_type[-1] == "Table":
                         tl_time = tl_time + table_times
                         tl_code = tl_code + [code for i in range(len(table_times))]
+                        tl_hour = tl_hour + [hour_log for i in range(len(table_times))]
                         tl_string = tl_string + table_strings
             
                 #Cleaning Variables
@@ -121,6 +136,7 @@ def main():
                 time = []
                 info_string = []
                 code = 0
+                hour_log = 0
                 data_type = []
                 f_regex  = []
                 sf_regex = []
@@ -130,11 +146,8 @@ def main():
                 candidate_code = (code_extractor.findall(line))[0]
                 if candidate_code in spec:
 
-#                    if candidate_code == "0xB193":
-#                        print "FOUND", candidate_code, "at line", line
-#                        raw_input("Continue?")
-
                     code = candidate_code
+                    hour_log = (hour_extractor.findall(line))[0]
                     state = "block found"
                     block_processing = True
                     
@@ -208,35 +221,58 @@ def main():
                                 table_times.append(v)
                                 table_strings.append(s)
                             
-        chunks_time = []
-        chunks_code = []
-        chunks_str  = []
+        chunks_time = [[]]
+        chunks_hour = [[]]
+        chunks_code = [[]]
+        chunks_str  = [[]]
         prev_marker = 0
+        cct = 0
+
+        startmodecounter = 0
         print "Dimension: ", len(tl_time)
         for i in range(1,len(tl_time)):
+            startmodecounter += 1
             if i%1000 == 0:
                 print "Currently at ", i
-            if (tl_time[i] - tl_time[i-1]) < -5000:
-                chunks_time.append(tl_time[prev_marker:(i-1)])
-                chunks_code.append(tl_code[prev_marker:(i-1)])
-                chunks_str.append(tl_string[prev_marker:(i-1)])
-                
-                prev_marker = i
-                a = zip(chunks_time[-1], chunks_code[-1], chunks_str[-1])
-                a.sort()
-                try:
-                    chunks_time[-1], chunks_code[-1], chunks_str[-1] = zip(*a)
-                except:
-                    chunks_time = chunks_time[0:-1]
-                    chunks_code = chunks_code[0:-1]
-                    chunks_str = chunks_str[0:-1]
+            
+            if tl_time[i] > 8000 and startmodecounter < 300:
+                chunks_time[cct-1].append(tl_time[i])
+                chunks_hour[cct-1].append(tl_hour[i])
+                chunks_code[cct-1].append(tl_code[i])
+                chunks_str[cct-1].append(tl_string[i])
 
-        #print chunks_time
-        #raw_input("Continue?")
+            elif (tl_time[i] - tl_time[i-1]) < -5000 and startmodecounter > 300:
+                chunks_time.append([])
+                chunks_hour.append([])
+                chunks_code.append([])
+                chunks_str.append([])
+                cct += 1
+                startmodecounter = 0
+                chunks_time[cct].append(tl_time[i])
+                chunks_hour[cct].append(tl_hour[i])
+                chunks_code[cct].append(tl_code[i])
+                chunks_str[cct].append(tl_string[i])
+
+            else:
+                chunks_time[cct].append(tl_time[i])
+                chunks_hour[cct].append(tl_hour[i])
+                chunks_code[cct].append(tl_code[i])
+                chunks_str[cct].append(tl_string[i])
+
+        for i in range(len(chunks_time)):
+            a = zip(chunks_time[i], chunks_hour[i], chunks_code[i], chunks_str[i])
+            a.sort()
+            try:
+                chunks_time[i], chunks_hour[i], chunks_code[i], chunks_str[i] = zip(*a)
+            except:
+                chunks_time = chunks_time[0:-1]
+                chunks_hour = chunks_hour[0:-1]
+                chunks_code = chunks_code[0:-1]
+                chunks_str = chunks_str[0:-1]
         
-        for t, c, s in zip(chunks_time, chunks_code, chunks_str):
-            for tm, cd, st in zip(t, c, s):
-                print >> outfile, "\t\t", tm, "\t\t", cd, "\t\t", st
+        for t, h, c, s in zip(chunks_time, chunks_hour, chunks_code, chunks_str):
+            for tm, ho, cd, st in zip(t, h, c, s):
+                print >> outfile, ho, "\t\t", tm, "\t\t", cd, "\t\t", st
 
 
 if __name__ == "__main__":
