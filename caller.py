71fc78c2834faac1d26b6778424c3f33f24f1daf
Fernando Pereira <fernandhenriqp@gmail.com>
2017-03-15 15:14:23 -0300

    Changed directories in caller.py
    Changed to reflet LCR quimera machine structure

--- caller.py
+++ caller.py
@@ -5,16 +5,18 @@ from subprocess import call
 import sys
 import os
 
-target_dir =  "/home/fpereira/QXDM/txt/"
-output_dir = "/home/fpereira/QXDM/proc/"
+target_dir =  "/home/fernandopereira/QXDM/txt/"
+output_dir = "/home/fernandopereira/QXDM/proc/"
 
 format_in  = ".txt"
 format_out = "PARSED.txt"
 
-op_path = "/home/fpereira/working/QXDM_Parser/QXDM_parser.py"
+op_dir  = "/home/fernandopereira/git_works/QXDM_parser/"
+op_path = op_dir + "QXDM_parser.py"
 
 for subdir, dirs, files in os.walk(target_dir, followlinks=True):
     for f in files:
         if f.endswith(format_in):
             tgtf = target_dir + f
-            call(["python", op_path, tgtf])
+            tgte = op_dir + "events.json"
+            call(["python", op_path, tgtf, tgte])
